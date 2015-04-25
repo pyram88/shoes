f9386d7ba81bc2fb6ef2e4a623af9119fa048cef
Jahdere <paul-louis.barbier@ingesup.com>
2015-04-25 15:23:30 +0200

    [ADD] includes footer / header

---
+++ includes/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="css/shoes.css"/>
+		<script type="text/javascript" src="js/site.js"></script>
+		<title> Choose the Shoes </title>
+	</head>
+	<body>
+		<div id="main">
+				<div id="title1">
+					<h1> Choose the shoes</h1>
+					<p id="sous-titre"> Ou les méandres d'une fille qui chaussait du 41</p>
+				</div>
+			<nav>
+				<ul id="menu">
+					<li>
+						<a href="#">Plus</a>
+						<ul>
+							<li><a href="contact.php">Contact</a></li>
+							<li><a href="merciqui.php">Merci qui?</a></li>
+						</ul>
+					</li>
+
+					<li>
+						<a href="#">Panier</a>
+					</li>	
+
+					<li>
+						<a href="#">Compte</a>
+						<ul>
+							<li><a href="profil.php">Salut Chaire EEE!</li>
+							<li><a href="#">Mes commandes</a></li>
+						</ul>
+					</li>	
+
+					<li>
+						<a href="chosetheshoes.php">Accueil</a>
+					</li>
+				</ul>
+				</br>
+			</nav>
